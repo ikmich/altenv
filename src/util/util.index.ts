cdfb2e4abb821fee1a920aa88a92bddd389fd9ce
ikmich <michfx@gmail.com>
2022-02-18 23:23:17 +0100

    Remove debugging comments

--- src/util/util.index.ts
+++ src/util/util.index.ts
@@ -5,10 +5,3 @@ export function _fn(f: () => any) {
 export function isNumeric(s: string) {
   return s != '' && !isNaN(Number(s));
 }
-
-// const value = '0.0.0.0';
-// console.log(`isNumeric(${value}):`, isNumeric(value));
-// // console.log(`parseInt('e')`, parseInt(''));
-// console.log(`Number(e)`, Number('e'));
-// // console.log(`parseFloat('')`, parseFloat(''));
-// console.log(`Number(${value})`, Number(value));
